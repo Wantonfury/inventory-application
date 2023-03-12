2d05754e0d2538420f31ea8d7883a4762bd27fb6
Wantonfury <orzan.alexandru94@gmail.com>
2023-03-12 13:43:34 +0200

    Add cors package and change route names

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 const itemController = require('../controllers/itemController');
 
-router.get('/get_categories', categoryController.get_categories);
+router.get('/categories', categoryController.get_categories);
 
-router.get('/monitors', itemController.get_items);
+router.get('/items', itemController.get_items);
 
-router.get('/monitor/:id', itemController.get_item);
+router.get('/item/:id', itemController.get_item);
 
 module.exports = router;
