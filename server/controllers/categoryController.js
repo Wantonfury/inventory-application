0c7703e2d9d955ef231d64dc14b460f112fa9d98
Wantonfury <orzan.alexandru94@gmail.com>
2023-03-16 23:04:50 +0200

    Return id with categories

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -2,7 +2,7 @@ const Category = require('../models/category')
 
 exports.get_categories = (req, res, next) => {
   Category.find()
-    .select({ name: 1, _id: 0 })
+    .select({ name: 1, _id: 1 })
     .then(categories => res.send(categories))
     .catch(err => next(err));
 }
