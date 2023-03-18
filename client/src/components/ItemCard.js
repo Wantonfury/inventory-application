6dd1901e58fcc4531494caa97b179611b79ef9a6
Wantonfury <orzan.alexandru94@gmail.com>
2023-03-18 18:45:41 +0200

    Display form validation errors and style the item-card

--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -3,6 +3,7 @@ import axios from "axios"
 import { useState } from "react";
 
 const ItemCard = (props) => {
+  const [errors, setErrors] = useState([]);
   const [formValue, setFormValue] = useState({
     name: props.item.name,
     description: props.item.description,
@@ -34,6 +35,8 @@ const ItemCard = (props) => {
     })
     .catch(err => {
       console.log(err);
+      
+      setErrors(err.response.data.errors);
     });
   }
   
@@ -46,30 +49,40 @@ const ItemCard = (props) => {
   
   return (
     <form id="item-form" className="item-card" method="POST" onSubmit={handleSubmit}>
-      <label htmlFor="item-name">Name: </label>
-      <textarea id="item-name" name="name" value={formValue.name} onChange={handleChange} />
+      <div className="item-card-textarea">
+        <label htmlFor="item-name">Name: </label>
+        <textarea rows="5" id="item-name" name="name" value={formValue.name} onChange={handleChange} />
+        
+        <label htmlFor="item-description">Description: </label>
+        <textarea rows="5" id="item-description" name="description" value={formValue.description} onChange={handleChange} />
+      </div>
       
-      <label htmlFor="item-description">Description: </label>
-      <textarea id="item-description" name="description" value={formValue.description} onChange={handleChange} />
+      <div className="item-card-options">
+        <label htmlFor="item-brand">Brand: </label>
+        <input type="text" id="item-brand" name="brand" value={formValue.brand} onChange={handleChange} />
+        
+        <label htmlFor="item-modelNo">Model Number: </label>
+        <input type="text" id="item-modelNo" name="modelNo" value={formValue.modelNo} onChange={handleChange} />
+        
+        <label htmlFor="item-category">Category: </label>
+        <select id="item-category" name="category" value={formValue.category} onChange={handleChange}>
+          {props.categories.map((category, index) => {
+            return <option key={index} value={category._id}>{category.name}</option>;
+          })}
+        </select>
+        
+        <label htmlFor="item-stock">Stock: </label>
+        <input type="text" id="item-stock" name="stock" value={formValue.stock} onChange={handleChange} />
+        
+        <label htmlFor="item-price">Price: </label>
+        <input type="text" id="item-price" name="price" value={formValue.price} onChange={handleChange} />
+      </div>
       
-      <label htmlFor="item-brand">Brand: </label>
-      <input type="text" id="item-brand" name="brand" value={formValue.brand} onChange={handleChange} />
-      
-      <label htmlFor="item-modelNo">Model Number: </label>
-      <input type="text" id="item-modelNo" name="modelNo" value={formValue.modelNo} onChange={handleChange} />
-      
-      <label htmlFor="item-category">Category: </label>
-      <select id="item-category" name="category" value={formValue.category} onChange={handleChange}>
-        {props.categories.map((category, index) => {
-          return <option key={index} value={category._id}>{category.name}</option>;
+      <ul className="item-card-errors">
+        {errors.map((error, index) => {
+          return <li key={index}>{error}</li>
         })}
-      </select>
-      
-      <label htmlFor="item-stock">Stock: </label>
-      <input type="text" id="item-stock" name="stock" value={formValue.stock} onChange={handleChange} />
-      
-      <label htmlFor="item-price">Price: </label>
-      <input type="text" id="item-price" name="price" value={formValue.price} onChange={handleChange} />
+      </ul>
       
       <button type="submit">{props.item.name !== '' ? 'Change' : 'Add'}</button>
     </form>
